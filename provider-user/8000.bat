7a393c6403fd780eb92461cbc11259a70f9572c3
hanyli <hanyli@yonyou.com>
2020-12-09 19:39:24 +0800

    启动脚本

---
+++ provider-user/8000.bat
@@ -0,0 +1,1 @@
+java -jar microservice-provider-user-1.0-SNAPSHOT.jar 
---
+++ provider-user/8001 - 副本 - 副本.bat
@@ -0,0 +1,1 @@
+java -jar microservice-provider-user-1.0-SNAPSHOT.jar --server.port=8004 --spring.application.name=provider-user
